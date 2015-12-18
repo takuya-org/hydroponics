860b0cee4f1d62a6498f570055c36238f5d6cff8
oniipon <b2142@oic.jp>
2015-12-19 02:26:05 +0900

    照度センサで値取って進む方向を判定するでゲソ

--- lux_sensor.py
+++ lux_sensor.py
@@ -1,5 +1,5 @@
-
-#!/usr/bin/env python
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import time
 import sys
@@ -8,32 +8,62 @@ import spidev
 spi = spidev.SpiDev()
 spi.open(0,0)
 
-def readAdc(channel):
-    adc = spi.xfer2([1,(8+channel)<<4,0])
-    data = ((adc[1]&3) << 8) + adc[2]
-    return data
+def read_channel(channel):
+  """
+  MCP3008経由でアナログセンサからのデータを受け取る。
+  channelはMCP3008の入力チャンネルで、0から7の値
+  """
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
+def decision_move(max_lux):
+  if max_lux == "cds0":
+    return [2,4,2]
+  elif max_lux == "cds1":
+    return [2,6,2]
+  elif max_lux == "cds2":
+    return [8,4,8]
+  elif max_lux == "cds3":
+    return [8,6,8]
 
-def convertVolts(data):
-    volts = (data * 3.3) / float(1023)
-    volts = round(volts,4)
-    return volts
 
-def convertTemp(volts):
-    temp = (100 * volts) - 50.0
-    temp = round(temp,4)
-    return temp
 
-if __name__ == '__main__':
-    try:
-        while True:
-            data = readAdc(0)
-            print("adc  : {:8} ".format(data))
-            volts = convertVolts(data)
-            temp = convertTemp(volts)
-            print("volts: {:8.2f}".format(volts))
-            print("temp : {:8.2f}".format(temp))
+while True:
 
-            time.sleep(1)
-    except KeyboardInterrupt:
-        spi.close()
-        sys.exit(0)
+  cds0 = read_channel(0)
+  cds1 = read_channel(1)
+  cds2 = read_channel(2)
+  cds3 = read_channel(3)
+  lux = {"cds0":cds0,
+         "cds1":cds1,
+         "cds2":cds2,
+         "cds3":cds3}
+  max_lux = max(lux, key=(lambda x: lux[x]))
+
+  print(lux)
+  print(max_lux)
+  print (decision_move(max_lux))
+
+
+
+
+
+
+
+#   hoge = min(cds0,cds1,cds2,cds3)   #最小値メモ　他との比較で使う用のやつ
+#   print "最小値：%s" % hoge
+#
+ #一番明るいのがどれかの判定　
+  # if hoge == cds0 :
+  #   print "左前 最小値：%s" % hoge
+  # if hoge == cds1 :
+  #   print "右前 最小値：%s" % hoge
+  # if hoge == cds2 :
+  #   print "左後 最小値：%s" % hoge
+  # if hoge == cds3 :
+  #   print "右後 最小値：%s" % hoge
+  #   print ""
+  #
+
+  #test
+  time.sleep(100)
