2336b2d7d1d455e4359f1e4f15bc20c76ab4293c
tanpopo <b2094@oic.jp>
2016-01-08 15:11:36 +0900

    修正分

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -1,13 +1,10 @@
 class Temperature(object):
-	DIR = '/sys/bus/w1/devices/w1_bus_master1/'
-	DEVICE_NAME = '28-011590281ff'
-	FILE_NAME = '/w1_slave'
-
-    def __init__(self):
-        pass
+    DIR = '/sys/bus/w1/devices/'
+    DEVICE_NAME = '28-0115902811ff'
+    FILE_NAME = '/w1_slave'
 
     def getTemprature(self):
-        with open(DIR + DEVICE + FILE_NAME) as f:
+        with open(Temperature.DIR + Temperature.DEVICE_NAME + Temperature.FILE_NAME) as f:
             data = f.read()
         temp = float(data[data.index('t=')+2:])/1000
         return temp
