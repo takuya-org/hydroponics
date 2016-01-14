c44fec1b29d4cad9b043b977249791a97f201b0e
yuzu <B2135@oic.jp>
2016-01-14 21:46:55 +0900

    スペルミス修正

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -5,7 +5,7 @@ class Temperature(object):
     DEVICE_NAME = '28-0115902811ff'
     FILE_NAME = '/w1_slave'
 
-    def getTemprature(self):
+    def getTemperature(self):
         with open(Temperature.DIR + Temperature.DEVICE_NAME + Temperature.FILE_NAME) as f:
             data = f.read()
         temp = float(data[data.index('t=')+2:])/1000
@@ -13,6 +13,6 @@ class Temperature(object):
 
     def storeTemperatureData(self):
         db_con = db_temperature.DbOperation()
-        temperature = self.getTemprature()
+        temperature = self.getTemperature()
         db_con.insertRecord(temperature)
         db_con.connectionClose()
