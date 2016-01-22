6b4d99f07a31c17fcbe2dd0cc62f6afe040dbcd0
tanpopo <b2094@oic.jp>
2016-01-22 12:58:17 +0900

    sql変更

--- db_temperature.py
+++ db_temperature.py
@@ -8,13 +8,14 @@ class DbOperation(object):
     def __init__(self):
         self.con = sqlite3.Connection(os.environ.get(DbOperation.PATH))
         c = self.con.cursor()
-        c.execute("""CREATE TABLE IF NOT EXISTS temp(date text, temp_c real)""")
+        c.execute("""CREATE TABLE IF NOT EXISTS temp(date text, time text, temp_c real)""")
 
     def insertRecord(self, temp):
         c = self.con.cursor()
         d = datetime.datetime.today()
         now_date = str(d.year) + "-" + str(d.month) + "-" + str(d.day)
-        c.execute("INSERT INTO temp(date, temp_c) VALUES(?, ?)", (now_date, temp))
+        now_time = str(d.hour) + ':' + str(d.second)
+        c.execute("INSERT INTO temp(date, now_time, temp_c) VALUES(?, ?, ?)", (now_date, now_time, temp))
         self.con.commit()
 
     def connectionClose(self):
